e75f7193fa043ef6d09bcb858e01546e84551cce
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-07-31 02:40:54 +0530

    add js file

--- src/Service/api.js
+++ src/Service/api.js
@@ -1,2 +1,14 @@
 const API_KEY = "ca60f6ef58d3c2c920997642e6513c1c";
 const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getpopularmovies = async () => {
+    const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+    const data = await response.json();
+    return data.results;
+};
+
+export const searchmovies = async (query) => {
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
+    const data = await response.json();
+    return data.results;
+};
