aadfd2797a101044a89e3f1ece42d0304d4561f5
Bhanuka10 <sahanbhanuka10@gmail.com>
2025-08-01 23:49:02 +0530

    completed the project

--- src/Pages/Favorite.jsx
+++ src/Pages/Favorite.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './Favorite.css'; 
+import { usemovieContext } from '../Component/Context/Moviecontext';
+import Moviescart from '../Component/Moviescart/Moviescart.jsx';
 
 function Favorite() {
+    const { favorites } = usemovieContext();
+    if (favorites && favorites.length > 0) {
+        return (
+            <div className="favorites">
+                <h2>Your Favorite Movies</h2>
+                <div className="movies-grid">
+                    {favorites.map((movie) => (
+                        <Moviescart key={movie.id} movies={movie} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
     return (
         <div className="favorites">
-            <h1>Your Favorite Movies</h1>
+            <h1>No Favorite Movies Yet</h1>
         </div>
     );
 }
